4571808961be22370326d4291ca10ef3f696132f
fedemodernell <modernellfederico@gmail.com>
2022-03-04 20:11:38 -0300

    Primer commit

---
+++ 01-introduccion/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var nombre1 string
+
+	nombre1 = "Hola Mundo persona"
+	nombre := 26.5
+
+	fmt.Println(nombre1)
+	fmt.Println(nombre)
+
+	nombre = 24
+	fmt.Println(nombre)
+
+	result := sumar(10, 20)
+	fmt.Println(result)
+}
+
+func sumar(a, b int) int {
+	return a + b
+}
+
